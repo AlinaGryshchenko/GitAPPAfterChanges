af82dedbbe852aef60ff439f4b866b4c0fa58ff7
AlinaGryshchenko <alinahryshchenkoios@gmail.com>
2023-01-23 17:22:12 +0000

    code 1

--- TestGitAlina/ViewController.swift
+++ TestGitAlina/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//code 1
+        
+        /*
+         
+         code1
+         code1
+         code1
+         
+         */
+
     }
 
 
